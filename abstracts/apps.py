2e0369a915fd896e4ae3b05facab7025b67d6299
spline <dmcemin@gmail.com>
2025-11-07 21:39:57 +0100

    apps files formated

--- abstracts/apps.py
+++ abstracts/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class AbstractsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'abstracts'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "abstracts"
--- integration/apps.py
+++ integration/apps.py
@@ -2,8 +2,8 @@ from django.apps import AppConfig
 
 
 class IntegrationConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'integration'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "integration"
 
     def ready(self):
-        import integration.signals
+        import integration.signals  # noqa
