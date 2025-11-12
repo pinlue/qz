459c8a9fba2fe9e0c6c7b0ff730bbb539a31e593
spline <dmcemin@gmail.com>
2025-11-12 19:54:24 +0100

    add topics admin

--- topics/admin.py
+++ topics/admin.py
@@ -1,6 +1,21 @@
 from django.contrib import admin
+from django.db.models import Count
 
 from topics.models import Topic
 
-# ToDo : Customize admin model
-admin.site.register(Topic)
+
+@admin.register(Topic)
+class TopicAdmin(admin.ModelAdmin):
+    list_display = ("name", "modules_count")
+    search_fields = ("name",)
+    ordering = ("name",)
+    fields = ("name",)
+    readonly_fields = ("modules_count",)
+    list_per_page = 50
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        return qs.annotate(_modules_count=Count("modules", distinct=True))
+
+    def modules_count(self, obj):
+        return getattr(obj, "_modules_count", 0)
