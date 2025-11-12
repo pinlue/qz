85f17fa4d9584abc2990e1fdaa47bdcbf8485483
spline <dmcemin@gmail.com>
2025-11-12 19:53:56 +0100

    add tasks admin

--- common/admin.py
+++ common/admin.py
@@ -1,6 +1,28 @@
 from django.contrib import admin
+from django.utils.html import format_html
 
 from common.models import ScheduledTask
 
-# ToDo : Customize admin model
-admin.site.register(ScheduledTask)
+
+@admin.register(ScheduledTask)
+class ScheduledTaskAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "status",
+        "scheduled_time",
+        "created",
+        "short_kwargs",
+    )
+    list_filter = ("status", "scheduled_time", "created")
+    list_editable = ("status",)
+    search_fields = ("name", "kwargs")
+    ordering = ("-scheduled_time",)
+    readonly_fields = ("created",)
+
+    def short_kwargs(self, obj):
+        text = str(obj.kwargs)
+        return text if len(text) < 60 else text[:57] + "..."
+
+    short_kwargs.short_description = "Kwargs"
+
+
