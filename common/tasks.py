670876ab9eb61a73abf09c66dd984ab1b975dcfd
spline <dmcemin@gmail.com>
2025-11-15 01:18:52 +0100

    add transaction to task runner func

--- common/tasks.py
+++ common/tasks.py
@@ -1,18 +1,25 @@
 from celery import shared_task
+from celery_singleton import Singleton
+from django.db import transaction
 from django.utils import timezone
 from celery import current_app
 
 from common.models import ScheduledTask
 
 
-@shared_task
+@shared_task(base=Singleton)
 def run_scheduled_tasks() -> None:
     now = timezone.now()
-    due_tasks = ScheduledTask.objects.filter(status="pending", scheduled_time__lte=now)
-    for task in due_tasks:
-        try:
-            current_app.send_task(task.name, kwargs=task.kwargs)
-            task.delete()
-        except Exception as _:
-            task.status = "failed"
-            task.save(update_fields=["status"])
+    with transaction.atomic():
+        due_tasks = (
+            ScheduledTask.objects
+            .select_for_update(skip_locked=True)
+            .filter(status="pending", scheduled_time__lte=now)
+        )
+        for task in due_tasks:
+            try:
+                current_app.send_task(task.name, kwargs=task.kwargs)
+                task.delete()
+            except Exception:
+                task.status = "failed"
+                task.save(update_fields=["status"])
