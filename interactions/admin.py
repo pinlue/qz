a460c024e737d05b9e03afe97bae187835bd0f6d
spline <dmcemin@gmail.com>
2025-11-15 01:09:23 +0100

    create interactions admin

--- interactions/admin.py
+++ interactions/admin.py
@@ -1,1 +1,23 @@
+from django.contrib import admin
 
+from interactions.models import Pin, Save
+
+
+class InteractionsAdminBase(admin.ModelAdmin):
+    list_filter = ("user", "content_type")
+    search_fields = ("user__username", "content_type__model")
+    ordering = ("user",)
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        return qs.select_related("user", "content_type")
+
+
+@admin.register(Pin)
+class PinAdmin(InteractionsAdminBase):
+    pass
+
+
+@admin.register(Save)
+class SaveAdmin(InteractionsAdminBase):
+    pass
