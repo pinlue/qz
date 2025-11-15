a21fc36012134002cff523db66040457ede4f694
spline <dmcemin@gmail.com>
2025-11-15 01:12:18 +0100

    create user admin

--- users/admin.py
+++ users/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 
 from users.models import User
+from django.contrib.auth.admin import UserAdmin
 
-# ToDo : Customize admin model
-admin.site.register(User)
+
+@admin.register(User)
+class UserAdmin(UserAdmin):
+    pass
