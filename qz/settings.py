2e6af843f24f59272421648f7d75798a355b1a56
spline <dmcemin@gmail.com>
2025-11-15 02:04:44 +0100

    add redis cache

--- qz/settings.py
+++ qz/settings.py
@@ -134,6 +134,13 @@ DATABASES = {
     }
 }
 
+# Cache
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/1",
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
