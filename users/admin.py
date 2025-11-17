b29ae25e4c3c8af7e913c552d9c62bbc02c08317
spline <dmcemin@gmail.com>
2025-11-17 22:03:55 +0100

    add avatar and bio fields to user admin

--- users/admin.py
+++ users/admin.py
@@ -1,9 +1,46 @@
 from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.admin import UserAdmin
 
 from users.models import User
-from django.contrib.auth.admin import UserAdmin
 
 
 @admin.register(User)
 class UserAdmin(UserAdmin):
-    pass
+    fieldsets = (
+        (None, {"fields": ("username", "password")}),
+        (
+            _("Personal info"),
+            {"fields": ("first_name", "last_name", "email", "bio", "avatar")},
+        ),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+    )
+
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "username",
+                    "usable_password",
+                    "password1",
+                    "password2",
+                    "bio",
+                    "avatar",
+                ),
+            },
+        ),
+    )
