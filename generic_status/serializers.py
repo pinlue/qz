16a39f5bb99fc5f140c0e00aa8fae2ee254308fd
spline <dmcemin@gmail.com>
2026-01-02 18:42:19 +0100

    add: PermListSerializer

--- generic_status/serializers.py
+++ generic_status/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 
 from generic_status.models import Learn, Rate, Perm
 from users.models import User
+from users.serializers import UserPublicSerializer
 
 
 class LearnSerializer(serializers.Serializer):
@@ -15,3 +16,11 @@ class RateSerializer(serializers.Serializer):
 class PermSerializer(serializers.Serializer):
     perm = serializers.ChoiceField(choices=Perm.Status.choices)
     user = serializers.PrimaryKeyRelatedField(queryset=User.objects.all())
+
+
+class PermListSerializer(serializers.ModelSerializer):
+    user = UserPublicSerializer(read_only=True)
+
+    class Meta:
+        model = Perm
+        fields = ["id", "user", "perm"]
